a0e26fe350a83d2ef1598500586a65ff36ead17b
Javier Martinez <javi.martinez.lopez@gmail.com>
2020-07-24 01:01:37 +0200

    draft of removing outliers function

--- script.R
+++ script.R
@@ -19,11 +19,30 @@ df01win<-read.table('sample_data_winter_2001.csv',header=T,sep=',')
 df09sum<-read.table('sample_data_summer_2009.csv',header=T,sep=',')
 df09win<-read.table('sample_data_winter_2009.csv',header=T,sep=',')
 
-df96sm<-melt(df96sum, id=c('year','season','lc'))
-df01sm<-melt(df01sum, id=c('year','season','lc'))
 
-s96 <- ggplot(df96sm, aes(as.factor(lc), value)) + geom_boxplot(aes(colour = season)) + facet_wrap(vars(variable), scales = "free")
+rmv_outl<-function(table){ # this must be done by each lc class!
+	df<-table
+	#print(head(df))
+ for(i in 3:32){
+	#print(i)
+	if(i!=28){
+		x<-sd(df[,i])
+		xneg<--1*x
+		#print(x)
+		df[abs(df[,i])>x,i]<-NA
+	}
+ }
+ return(df)
+}
 
-s01 <- ggplot(df01sm, aes(as.factor(lc), value)) + geom_boxplot(aes(colour = season)) + facet_wrap(vars(variable), scales = "free")
+s96df<-rmv_outl(df96sum)
+
+
+df96sm<-melt(s96df, id=c('year','season','lc'),na.rm=T)
+df01sm<-melt(s01df, id=c('year','season','lc'),na.rm=T)
+
+s96plot <- ggplot(df96sm, aes(as.factor(lc), value)) + geom_boxplot(aes(colour = season)) + facet_wrap(vars(variable), scales = "free")
+
+s01plot <- ggplot(df01sm, aes(as.factor(lc), value)) + geom_boxplot(aes(colour = season)) + facet_wrap(vars(variable), scales = "free")
 
 
